1b49f243edbcc07b998b74d28ab0bcacd7653bd2
ordenhub <97398164+ordenhub@users.noreply.github.com>
2022-01-09 19:14:51 +0300

    Update OrdenHub.lua

--- OrdenHub.lua
+++ OrdenHub.lua
@@ -105,6 +105,11 @@ local function ZBYZ_fake_script() -- Load.LocalScript
 		wait(2)
 		script.Parent.Parent.Parent:Destroy()
 		loadstring(game:HttpGet(('https://raw.githubusercontent.com/ordenhub/ordenhub/main/pass-squid'),true))()
+	elseif game.PlaceId == 2414851778 then
+		script.Parent.Parent.TextLabel.Text = "Impossible Squid game"
+		wait(2)
+		script.Parent.Parent.Parent:Destroy()
+		loadstring(game:HttpGet(('https://raw.githubusercontent.com/ordenhub/ordenhub/main/pass-dungeon.lua'),true))()
 	else
 		script.Parent.Parent.TextLabel.Text = "Error"
 		wait(2)
